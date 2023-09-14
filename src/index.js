06525b13fe82409a2ba1075ffe7eebe7d2e871a5
Stanislav-i <itandeducationua@gmail.com>
2023-09-14 14:32:21 +0300

    test 2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <Provider store={store}>
-    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+    <BrowserRouter basename="/react-phonebook">
       <App />
     </BrowserRouter>
     </Provider>
